6501aef5617a07ad8543057c8bd87b52fb536599
Hasoo786 <hk51413121@gmail.com>
2024-09-28 04:29:04 +0500

    object

---
+++ app.js
@@ -0,0 +1,55 @@
+// object
+
+// collection of key and value is called object
+
+
+// const obj = {
+
+//         username : "Haseeb",
+//         email : "Hk@gmail.com",
+//         age : 22,
+//         roll : 342342,
+
+// }
+
+// // console.log(obj["username"]);
+// // console.log(obj["email"]);
+// // console.log(obj["age"]);
+
+
+// obj.username = "khan"
+// obj.email = "KH@gmail.com"
+// obj.age = 21
+// delete obj.roll
+
+// console.log(obj);
+
+
+
+
+
+
+// let obj = {
+
+//     Username : "Khan" ,
+//     Email : "KH@gmail.com" ,
+//     Age : 22 ,
+//     rollNum : 34534 ,
+
+// }
+
+// // console.log(obj["Username"]);
+// // console.log(obj["Email"]);
+// // console.log(obj["rollNum"]);
+// // console.log(obj["Age"]);
+
+// obj.Username = "Haseeb"
+// obj.Email = "HK@gmail.com"
+// obj.Age = 21
+// obj.rollNum = 345323
+
+// console.log(obj);
+
+
+
+
